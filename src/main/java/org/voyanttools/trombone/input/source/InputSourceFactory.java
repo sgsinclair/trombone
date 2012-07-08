b9d858a51c7ab0f63a0ac64acfdb758d68017d23
sgsinclair <sgsinclair@gmail.com>
2012-07-07 20:54:14 -0700

    added copyright header to individual code files

--- src/main/java/org/voyanttools/trombone/input/source/InputSourceFactory.java
+++ src/main/java/org/voyanttools/trombone/input/source/InputSourceFactory.java
@@ -1,6 +1,24 @@
-/**
+/*******************************************************************************
+ * Trombone is a flexible text processing and analysis library used
+ * primarily by Voyant Tools (voyant-tools.org).
  * 
- */
+ * Copyright (©) 2007-2012 Stéfan Sinclair & Geoffrey Rockwell
+ * 
+ * This file is part of Trombone.
+ * 
+ * Trombone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Trombone.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package org.voyanttools.trombone.input.source;
 
 /**
