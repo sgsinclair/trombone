ab9eb315ee0b337640c2b92ee3056a0e89bb1a9f
sgsinclair <sgsinclair@gmail.com>
2017-04-30 13:52:04 -0400

    notebook saving

--- src/main/java/org/voyanttools/trombone/tool/notebook/NotebookManager.java
+++ src/main/java/org/voyanttools/trombone/tool/notebook/NotebookManager.java
@@ -11,6 +11,7 @@ import java.net.URISyntaxException;
 import org.apache.cxf.helpers.IOUtils;
 import org.voyanttools.trombone.input.source.UriInputSource;
 import org.voyanttools.trombone.storage.Storage;
+import org.voyanttools.trombone.storage.Storage.Location;
 import org.voyanttools.trombone.tool.utils.AbstractTool;
 import org.voyanttools.trombone.util.FlexibleParameters;
 
@@ -26,7 +27,7 @@ public class NotebookManager extends AbstractTool {
 	String notebook = null; // notebook source (ID, URL, etc.)
 	
 	String jsonData = null; // notebook data as JSON
-
+	
 	public NotebookManager(Storage storage, FlexibleParameters parameters) {
 		super(storage, parameters);
 	}
@@ -53,9 +54,14 @@ public class NotebookManager extends AbstractTool {
 				} finally {
 					if (is!=null) is.close();
 				}
+			} else if (jsonData==null && storage.isStored(notebook, Location.notebook)) {
+				jsonData = storage.retrieveString(notebook, Storage.Location.notebook);
 			}
 		} else if (jsonData!=null) { // we have sent data but no notebook, so we're saving
-//			id = storage.storeString(string))
+			// assign new notebook ID
+			notebook = storage.storeString(jsonData, Location.notebook);
+			jsonData = null;
+			return;
 		}
 		if (jsonData==null) {
 			throw new RuntimeException("Unable to locate requested notebook.");
