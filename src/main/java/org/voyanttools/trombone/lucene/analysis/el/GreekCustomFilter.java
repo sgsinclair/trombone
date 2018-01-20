42e0c88450e688869799cf9cfb847eef1c2b84fc
Stéfan Sinclair <sgsinclair@gmail.com>
2018-01-20 09:24:55 -0500

    making sure Greek filter normalizes and lowercases

--- src/main/java/org/voyanttools/trombone/lucene/analysis/el/GreekCustomFilter.java
+++ src/main/java/org/voyanttools/trombone/lucene/analysis/el/GreekCustomFilter.java
@@ -7,7 +7,7 @@ import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
 /**
- * Normalizes token text to use tonos rather than oxia accent, see
+ * Normalizes token text to use tonos rather than oxia accent and normalize sigmas, see
  * 		https://github.com/aurelberra/stopwords/blob/master/revision_notes.md#precombined-diacritics
  */
 public final class GreekCustomFilter extends TokenFilter {
@@ -38,30 +38,39 @@ public final class GreekCustomFilter extends TokenFilter {
   
   private int lowerCase(int codepoint, boolean isLast) {
     switch(codepoint) {
-    		// see https://github.com/aurelberra/stopwords/blob/master/revision_notes.md#precombined-diacritics
-		// and https://github.com/sgsinclair/Voyant/issues/382#issuecomment-358821892
+		// normalize sigmas, see https://github.com/sgsinclair/Voyant/issues/382#issuecomment-358821892
     		case '\u03C3': // small sigma
     		case '\u03A3': // upper sigma
 		case '\u03C2': // small final sigma 
 		case '\u03F2': // lunate sigma
 		case '\u03F9': // upper lunate sigma
 			return isLast ? '\u03C2' /* small final sigma */ : '\u03C3' /*small sigma */;
-	    case '\u1F71': return '\u03AC'; // Lowercase Alpha + acute 
-	    case '\u1FBB': return '\u0386'; // Uppercase Alpha + acute 
-	    case '\u1F73': return '\u03AD'; // Lowercase Epsilon + acute 
-	    case '\u1FC9': return '\u0388'; // Uppercase Epsilon + acute 
-	    case '\u1F75': return '\u03AE'; // Lowercase Eta + acute 
-	    case '\u1FCB': return '\u0389'; // Uppercase Eta + acute 
-	    case '\u1F77': return '\u03AF'; // Lowercase Iota + acute 
-	    case '\u1FDB': return '\u038A'; // Uppercase Iota + acute 
-	    case '\u1F79': return '\u03CC'; // Lowercase Omicron + acute 
-	    case '\u1FF9': return '\u038C'; // Uppercase Omicron + acute 
-	    case '\u1F7B': return '\u03CD'; // Lowercase Upsilon + acute 
-	    case '\u1FEB': return '\u038E'; // Uppercase Upsilon + acute 
-	    case '\u1F7D': return '\u03CE'; // Lowercase Omega + acute 
-	    case '\u1FFB': return '\u038F'; // Uppercase Omega + acute 
+			
+		// normalize oxia to tonos, see https://github.com/aurelberra/stopwords/blob/master/revision_notes.md#precombined-diacritics
+	    case '\u1F71': // Lowercase Alpha + acute 
+	    case '\u1FBB':  // Uppercase Alpha + acute 
+	    		return '\u03AC';
+	    case '\u1F73': // Lowercase Epsilon + acute 
+	    case '\u1FC9': // Uppercase Epsilon + acute 
+	    		return '\u03AD';
+	    case '\u1F75': // Lowercase Eta + acute 
+	    case '\u1FCB': // Uppercase Eta + acute 
+	    		return '\u03AE';
+	    case '\u1F77': // Lowercase Iota + acute 
+	    case '\u1FDB': // Uppercase Iota + acute 
+	    		return '\u03AF';
+	    case '\u1F79': // Lowercase Omicron + acute 
+	    case '\u1FF9': // Uppercase Omicron + acute 
+	    		return '\u03CC';
+	    case '\u1F7B': // Lowercase Upsilon + acute 
+	    case '\u1FEB': // Uppercase Upsilon + acute
+	    		return '\u03CD';
+	    case '\u1F7D': // Lowercase Omega + acute 
+	    case '\u1FFB': // Uppercase Omega + acute
+	    		return '\u03CE';
 	    case '\u1FD3': return '\u0390'; // Lowercase Iota + dialytika + acute 
 	    case '\u1FE3': return '\u03B0'; // Lowercase Upsilon + dialytika + acute 
+	    
 	    default: return Character.toLowerCase(codepoint);
     }
   }
