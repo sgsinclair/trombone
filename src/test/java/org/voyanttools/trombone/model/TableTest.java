5a29adbd6820abd721826236877618a00d36d72c
Andrew James <ajmacdonald@users.noreply.github.com>
2021-02-16 11:31:30 -0500

    some table tests

--- src/test/java/org/voyanttools/trombone/model/TableTest.java
+++ src/test/java/org/voyanttools/trombone/model/TableTest.java
@@ -3,6 +3,10 @@
  */
 package org.voyanttools.trombone.model;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
 import org.junit.Test;
 import org.voyanttools.trombone.model.table.Table;
 
@@ -11,12 +15,11 @@ import org.voyanttools.trombone.model.table.Table;
  *
  */
 public class TableTest {
-
+	
 	@Test
 	public void test() {
-		Table table;
-		
-		table = new Table("zero	one	two	three\na	0	1	2\nb	0	2	1", Table.Format.tsv, true, true);
+		Table table = new Table("zero	one	two	three\na	0	1	2\nb	0	2	1", Table.Format.tsv, true, true);
+		assertEquals("1", table.getColumn(2)[1]);
 	}
 
 }
--- src/test/java/org/voyanttools/trombone/tool/TableCorrelationsTest.java
+++ src/test/java/org/voyanttools/trombone/tool/TableCorrelationsTest.java
@@ -1,17 +1,57 @@
 package org.voyanttools.trombone.tool;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.junit.Test;
+import org.voyanttools.trombone.storage.Storage;
+import org.voyanttools.trombone.util.FlexibleParameters;
+import org.voyanttools.trombone.util.TestHelper;
 
 public class TableCorrelationsTest {
 
-	//@Test
+	@Test
 	public void test() throws IOException {
-//		Storage storage = new MemoryStorage();
-//		FlexibleParameters parameters = new FlexibleParameters();
-
-
+		for (Storage storage : TestHelper.getDefaultTestStorages()) {
+			System.out.println("Testing with "+storage.getClass().getSimpleName()+": "+storage.getLuceneManager().getClass().getSimpleName());
+			test(storage);
+		}
+	}
+	
+	public void test(Storage storage) throws IOException {
+		FlexibleParameters parameters = new FlexibleParameters();
+		parameters.addParameter("input", "a\tb\n20\t12\n19\t11\n16\t8\n10\t4\n2\t-1");
+		parameters.addParameter("inputFormat", "tsv");
+		parameters.addParameter("columnHeaders", "true");
+		parameters.addParameter("implementation", "pearson");
+		
+		TableCorrelations tc = new TableCorrelations(storage, parameters);
+		tc.run();
+		
+		assertEquals(0.9957, tc.getCorrelation(), 0.0001);
+		
+		parameters = new FlexibleParameters();
+		parameters.addParameter("input", "a\tb\n20\t12\n19\t11\n16\t8\n10\t4");
+		parameters.addParameter("inputFormat", "tsv");
+		parameters.addParameter("columnHeaders", "true");
+		parameters.addParameter("implementation", "spearman");
+		
+		tc = new TableCorrelations(storage, parameters);
+		tc.run();
+		
+		assertEquals(1, tc.getCorrelation(), 0.0001);
+		
+		parameters = new FlexibleParameters();
+		parameters.addParameter("input", "a\tb\n20\t12\n19\t11\n16\t8\n10\t4");
+		parameters.addParameter("inputFormat", "tsv");
+		parameters.addParameter("columnHeaders", "true");
+		parameters.addParameter("implementation", "kendall");
+		
+		tc = new TableCorrelations(storage, parameters);
+		tc.run();
+		
+		assertEquals(1, tc.getCorrelation(), 0.0001);
 	}
 
 }
